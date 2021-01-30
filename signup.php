627dc22d04019857e9af1c81d45ff608037ae62f
aasmal97 <74555081+aasmal97@users.noreply.github.com>
2021-01-30 17:31:14 -0500

    'update69

--- signup.php
+++ signup.php
@@ -397,12 +397,19 @@ if ( isset($_POST["teacher_signup"]) ) {
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     <script>
-      $('#signup-form-toggler').on('click',function() {
-        $("#student-signup-form").toggleClass("collapse.show");
-        $("#teacher-signup-form").toggleClass("collapse.show");
-        $("#student-signup-form").toggleClass("collapse");
-        $("#teacher-signup-form").toggleClass("collapse");
-      });
+    $('#student-signup-btn').on('click',function() {
+      $("#student-signup-form").addClass("collapse.show");
+      $("#student-signup-form").removeClass("collapse");
+      $("#teacher-signup-form").removeClass("collapse.show");
+      $("#teacher-signup-form").addClass("collapse")
+    });
+
+    $('#teacher-signup-btn').on('click',function() {
+      $("#teacher-signup-form").addClass("collapse.show");
+      $("#teacher-signup-form").removeClass("collapse");
+      $("#student-signup-form").removeClass("collapse.show");
+      $("#student-signup-form").addClass("collapse")
+    });
   </script>
 </body>
 
